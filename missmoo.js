117fe1885d9fdae3c64eb76557351372c2133836
Matt Rider <mattwr18@gmail.com>
2017-08-16 00:26:22 -0300

    Update to save to localStorage

--- missmoo.js
+++ missmoo.js
@@ -13,7 +13,8 @@ $(document).ready(function () {
             var findAlmond = storage.find(function(key) {
               return key.name === "Almond"});
               var $btnClicked = "";
-
+              var questionProfit = "What were the total sales/expenses for this week";
+              var questionStock = "How much product did you use/purchase this week?";
               $("#peanut").html(findPeanut.name + ":<br>" + findPeanut.profit);
               $("#sunflower").html(findSunflower.name + ":<br>" + findSunflower.profit);
               $("#yam").html(findYam.name + ":<br>" + findYam.profit);
@@ -28,47 +29,59 @@ $(document).ready(function () {
               $("#coconutStock").html(findCoconut.name + ":<br>" + findCoconut.stock);
               $("#almondStock").html(findAlmond.name + ":<br>" + findAlmond.stock);
 
-              $("#peanut").click(function(){
-                $("#questions").html("What were the total sales/expenses for this week?");
-                $(".form-group").removeAttr("style");
-                $(this).addClass("btn-danger");
-                btnClicked = "Peanut";
+              $("#peanut").click(function (){
+                afterBtnClick(this, questionProfit);
+                $btnClicked = "Peanut";
               });
-
               $("#sunflower").click(function(){
-                $("#questions").html("What were the total sales/expenses for this week?");
-                $(".form-group").removeAttr("style");
-                $(this).addClass("btn-danger");
-                btnClicked = "Sunflower";
+                afterBtnClick(this, questionProfit);
+                $btnClicked = "Sunflower";
               });
               $("#yam").click(function(){
-                $("#questions").html("What were the total sales/expenses for this week?");
-                $(".form-group").removeAttr("style");
-                $(this).addClass("btn-danger");
-                btnClicked = "Yam";
+                afterBtnClick(this, questionProfit);
+                $btnClicked = "Yam";
               });
               $("#cashew").click(function(){
-                $("#questions").html("What were the total sales/expenses for this week?");
-                $(".form-group").removeAttr("style");
-                $(this).addClass("btn-danger");
-                btnClicked = "Cashew";
+                afterBtnClick(this, questionProfit);
+                $btnClicked = "Cashew";
               });
               $("#coconut").click(function(){
-                $("#questions").html("What were the total sales/expenses for this week?");
-                $(".form-group").removeAttr("style");
-                $(this).addClass("btn-danger");
-                btnClicked = "Coconut";
+                afterBtnClick(this, questionProfit);
+                $btnClicked = "Coconut";
               });
               $("#almond").click(function(){
-                $("#questions").html("What were the total sales/expenses for this week?");
-                $(".form-group").removeAttr("style");
-                $(this).addClass("btn-danger");
-                btnClicked = "Almond";
+                afterBtnClick(this, questionProfit);
+                $btnClicked = "Almond";
+              });
+              $("#peanutStock").click(function(){
+                afterBtnClick(this, questionStock);
+                $btnClicked = "PeanutStock";
+              });
+              $("#sunflowerStock").click(function(){
+                afterBtnClick(this, questionStock);
+                $btnClicked = "SunflowerStock";
+              });
+              $("#yamStock").click(function(){
+                afterBtnClick(this, questionStock);
+                $btnClicked = "YamStock";
+              });
+              $("#cashewStock").click(function(){
+                afterBtnClick(this, questionStock);
+                $btnClicked = "CashewStock";
+              });
+              $("#coconutStock").click(function(){
+                afterBtnClick(this, questionStock);
+                $btnClicked = "CoconutStock";
+              });
+              $("#almondStock").click(function(){
+                afterBtnClick(this, questionStock);
+                $btnClicked = "AlmondStock";
               });
 
               $("#submitBtn").click(function () {
                 var submit = $("#number-input").val();
-                  switch (btnClicked) {
+                    $("#number-input").val(0);
+                    switch ($btnClicked) {
                     case "Peanut":
                     findPeanut.profit += Number(submit);
                     $("#peanut").html(findPeanut.name + ":<br>" + findPeanut.profit);
@@ -105,13 +118,60 @@ $(document).ready(function () {
                     $("#almond").removeClass("btn-danger");
 
                     break;
+                    case "PeanutStock":
+                    findPeanut.stock += Number(submit);
+                    $("#peanutStock").html(findPeanut.name + ":<br>" + findPeanut.stock);
+                    $("#peanutStock").removeClass("btn-danger");
+
+                    break;
+                    case "SunflowerStock":
+                    findSunflower.stock += Number(submit);
+                    $("#sunflowerStock").html(findSunflower.name + ":<br>" + findSunflower.stock);
+                    $("#sunflowerStock").removeClass("btn-danger");
+
+                    break;
+                    case "YamStock":
+                    findYam.stock += Number(submit);
+                    $("#yamStock").html(findYam.name + ":<br>" + findYam.stock);
+                    $("#yamStock").removeClass("btn-danger");
+
+                    break;
+                    case "CashewStock":
+                    findCashew.stock += Number(submit);
+                    $("#cashewStock").html(findCashew.name + ":<br>" + findCashew.stock);
+                    $("#cashewStock").removeClass("btn-danger");
+
+                    break;
+                    case "CoconutStock":
+                    findCoconut.stock += Number(submit);
+                    $("#coconutStock").html(findCoconut.name + ":<br>" + findCoconut.stock);
+                    $("#coconutStock").removeClass("btn-danger");
+
+                    break;
+                    case "AlmondStock":
+                    findAlmond.stock += Number(submit);
+                    $("#almondStock").html(findAlmond.name + ":<br>" + findAlmond.stock);
+                    $("#almondStock").removeClass("btn-danger");
+
+                    break;
 
                     default: alert("OH NO!!");
                   }
+//Save data to localStorage
+                  var productsArray = [];
+                  productsArray.push(findPeanut)
+                  productsArray.push(findYam)
+                  productsArray.push(findAlmond)
+                  productsArray.push(findCoconut)
+                  productsArray.push(findSunflower)
+                  productsArray.push(findCashew);
+                  localStorage.setItem("matt", JSON.stringify(productsArray));
                 });
-                $(window).click(function revertBg(element) {
-                  if (element.target.matches('button')) {
-                      console.log(element);
-                  }
-                });
+//Code common to all btn clicks
+                function afterBtnClick(btn, quest) {
+                  $("#questions").html(quest);
+                  $(".form-group").removeAttr("style");
+                  $(btn).addClass("btn-danger");
+
+                }
               });
